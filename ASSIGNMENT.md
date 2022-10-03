6d97c7856a44b0a1fc8075a7e1efe1aff143ff4b
Vankayalapati <Thirupathiswamy.Vankayalapati@lumen.com>
2022-10-03 15:52:21 +0530

    Code changes for First Commit

---
+++ ASSIGNMENT.md
@@ -0,0 +1,13 @@
+### Implement following commands
+- AddGroup
+- AddMember
+- Groups
+
+### Next Class
+- Implementing settle up
+
+
+### Splitwise 3 Assignments 3
+- Implement command for settling group
+- Implement the controller for settling group
+- Implement settling a user
---
+++ src/main/java/com/scaler/splitwisejune22/controllers/UserController.java
@@ -0,0 +1,44 @@
+package com.scaler.splitwisejune22.controllers;
+
+import com.scaler.splitwisejune22.dtos.RegisterUserRequestDto;
+import com.scaler.splitwisejune22.dtos.RegisterUserResponseDto;
+import com.scaler.splitwisejune22.dtos.UpdateProfileRequestDto;
+import com.scaler.splitwisejune22.dtos.UpdateProfileResponseDto;
+import com.scaler.splitwisejune22.models.User;
+import com.scaler.splitwisejune22.services.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    public RegisterUserResponseDto registerUser(RegisterUserRequestDto request) {
+        User user = userService.registerUser(
+                request.getPhoneNumber(),
+                request.getPassword(),
+                request.getUsername()
+        );
+
+        RegisterUserResponseDto registerUserResponseDto = new RegisterUserResponseDto();
+        registerUserResponseDto.setUser(user);
+        return registerUserResponseDto;
+    }
+
+    public UpdateProfileResponseDto updateProfile(UpdateProfileRequestDto request) {
+        User user = userService.updateProfile(
+                request.getUserId(),
+                request.getNewPassword()
+        );
+
+        UpdateProfileResponseDto response = new UpdateProfileResponseDto();
+        response.setUser(user);
+        return response;
+    }
+
+}
